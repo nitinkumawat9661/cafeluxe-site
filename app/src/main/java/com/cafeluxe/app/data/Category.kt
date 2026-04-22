c065b9f293cdb12f7c279e65abbd9eb7b57ee4c1
nitinkumawat9661 <nitinkumawat985@gmail.com>
2026-04-22 20:56:22 +0530

    Remove Android artifacts from website repo

--- app/src/main/java/com/cafeluxe/app/data/Category.kt
+++
@@ -1,8 +0,0 @@
-package com.cafeluxe.app.data
-
-data class Category(
-    val id: String = "",
-    val name: String = "",
-    val order: Int = 0,
-    val isActive: Boolean = true
-)
--- app/src/main/java/com/cafeluxe/app/data/TableModel.kt
+++
@@ -1,8 +0,0 @@
-package com.cafeluxe.app.data
-
-data class TableModel(
-    val id: String = "",
-    val name: String = "",
-    val tableCode: String = "",
-    val isActive: Boolean = true
-)
